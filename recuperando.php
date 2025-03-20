bbc41972cf87d31eefce4dd2cfcaacfad8c4c3d3
Vinícius Bispo <77473673+vinicius158@users.noreply.github.com>
2025-03-19 22:50:08 -0300

    Update recuperando.php

--- recuperando.php
+++ recuperando.php
@@ -33,7 +33,9 @@
 
     </div>      
 
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>              
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>   
+
+    <script src="http://jqueryvalidation.org/files/dist/jquery.validate.js"></script> 
     
     <script src = "js/recuperando.js"></script>                                        
 
